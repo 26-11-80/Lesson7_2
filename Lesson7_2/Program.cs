0bd465627c887d350065125f3391f8118436d5c3
anvarovagulnaz <anvarovagulnaz@gmail.com>
2021-12-04 15:49:20 +0500

    Добавьте файлы проекта.

---
+++ Lesson7_2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcCube(10, out int S, out int V);
+            Console.Write("Площадь куба ");
+            Console.WriteLine(S);
+            Console.Write("Объем куба ");
+            Console.WriteLine(V);
+            Console.ReadKey();
+        }
+
+        static void CalcCube(int a, out int S, out int V)
+        {
+            S = 6 * a * a;
+            V = a * a * a;
+        }
+    }
+}
+
+        
+        
+          
+            
+        
